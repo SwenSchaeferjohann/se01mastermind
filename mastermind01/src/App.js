bded3e3984abd85edfa002a4da8a2d29566d8137
SwenSchaeferjohann <swen.schaeferjohann@code.berlin>
2020-06-04 11:18:57 +0200

    add main function and basic layout

--- mastermind01/src/App.js
+++ mastermind01/src/App.js
@@ -1,7 +1,43 @@
-import React from 'react'
+import React, { useState, useEffect, Fragment } from 'react'
 
-function App() {
-  return 'hello Fabio'
+export default function App() {
+  return MasterMind()
 }
 
-export default App
+function MasterMind() {
+  const [guesses, setGuesses] = useState([])
+  const code = 4431
+
+  return (
+    <Fragment>
+      <div>
+        Welcome to Mastermind! I created a random code for you. Let's get
+        crackin'
+      </div>
+      {guesses.map((guess, index) => (
+        <div>
+          Guess #{index + 1}: {guess}{' '}
+        </div>
+      ))}
+      <Guess1 />
+    </Fragment>
+  )
+}
+
+const Guess1 = () => {
+  console.log('first guess')
+  return (
+    <Fragment>
+      <div>Please enter your first guess (XXXX)</div>
+      <form onSubmit={(e) => handleGuessSubmit(e)}>
+        <input autoFocus></input>
+        <button type='submit'>Guess!</button>
+      </form>
+    </Fragment>
+  )
+}
+
+const handleGuessSubmit = (e) => {
+  e.preventDefault()
+  return console.log('submitted')
+}
