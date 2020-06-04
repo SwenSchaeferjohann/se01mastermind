2ac8018c904d2db6509dba6758334075eae81640
SwenSchaeferjohann <swen.schaeferjohann@code.berlin>
2020-06-04 13:35:36 +0200

    final, comments, winloose msg, tested

--- mastermind01/src/App.js
+++ mastermind01/src/App.js
@@ -5,7 +5,7 @@ export default function App() {
 }
 
 function MasterMind() {
-  const [counter, setCounter] = useState(0)
+  const [counter, setCounter] = useState(0) // tracks current guess
   const [guesses, setGuesses] = useState([
     { try: '', hint: '' },
     { try: '', hint: '' },
@@ -20,60 +20,89 @@ function MasterMind() {
     { try: '', hint: '' },
     { try: '', hint: '' },
   ])
+  const [winLoose, setWinLoose] = useState(null) // determines if lost or won messsage should be displayed
 
-  const code = '4431'
+  var code = '4242'
+  // useEffect(() => {
+  //   var r1 = Math.floor(Math.random() * 6 + 1)
+  //   console.log('r1: ', r1)
+  //   var r2 = Math.floor(Math.random() * 6 + 1)
+  //   var r3 = Math.floor(Math.random() * 6 + 1)
+  //   var r4 = Math.floor(Math.random() * 6 + 1)
+
+  //   var whole = String(r1) + String(r2) + String(r3) + String(r4)
+  //   console.log('whole: ', whole)
+
+  //   code = whole
+  // }, [])
+  // console.log('code: ', code)
   // console.log('guesses state', guesses)
+
   return (
     <Fragment>
-      <div>
-        Welcome to Mastermind! I created a random code for you. Let's get
-        crackin'
-      </div>
-      {guesses.map((guess, index) => {
-        let number = index + 1
-        // console.log(counter)
-        // console.log(index)
-        return (
-          <div
-            style={{
-              display: 'flex',
-              color: counter === index ? 'blue' : 'grey',
-            }}
-          >
-            {
-              // counter === index + 1 &&
-              <div>{'Guess #' + number + ': ' + guess.try}</div>
-            }
-            {guess.hint.length > 0 && (
-              <div style={{ marginLeft: '1rem' }}>
-                {'You got ' + guess.hint + ' color/s right!'}
-              </div>
-            )}
+      {winLoose === null ? (
+        <Fragment>
+          <div>
+            Welcome to Mastermind! I created a random code for you. Let's get
+            crackin'
           </div>
-        )
-      })}
-      <GuessInput
-        key={counter}
-        setValue={(value) => {
-          // console.log('value: ', value)
-          let newGuess = [...guesses]
-          newGuess[counter].try = value
-          setGuesses(newGuess)
-        }}
-        value={guesses[counter].try}
-        counter={counter}
-        setCounter={setCounter}
-        code={code}
-        setHint={(hint) => {
-          let newGuess = [...guesses]
-          newGuess[counter].hint = hint
-          setGuesses(newGuess)
-        }}
-      />
+          {/* Render list of all states of each guess */}
+          {guesses.map((guess, index) => {
+            let number = index + 1
+            // console.log(counter)
+            // console.log(index)
+            return (
+              <div
+                style={{
+                  display: 'flex',
+                  color: counter === index ? 'blue' : 'grey',
+                }}
+              >
+                {
+                  // counter === index + 1 &&
+                  <div>{'Guess #' + number + ': ' + guess.try}</div>
+                }
+                {guess.hint.length > 0 && (
+                  <div style={{ marginLeft: '1rem' }}>
+                    {guess.try.length === 4 &&
+                      'You got ' + guess.hint + ' color/s right!'}
+                  </div>
+                )}
+              </div>
+            )
+          })}
+          {/* render the current input field */}
+          <GuessInput
+            key={counter}
+            setValue={(value) => {
+              // console.log('value: ', value)
+              let newGuess = [...guesses]
+              newGuess[counter].try = value
+              setGuesses(newGuess)
+            }}
+            value={guesses[counter].try}
+            counter={counter}
+            setCounter={setCounter}
+            code={code}
+            setHint={(hint) => {
+              let newGuess = [...guesses]
+              newGuess[counter].hint = hint
+              setGuesses(newGuess)
+            }}
+            winLoose={winLoose}
+            setWinLoose={setWinLoose}
+          />
+        </Fragment>
+      ) : winLoose === true ? (
+        <div> You won! refresh the page to play again.</div>
+      ) : (
+        <div>You lost!refresh the page to play again. </div>
+      )}
     </Fragment>
   )
 }
 
+// layout of each input field
 const GuessInput = ({
   value,
   setValue,
@@ -81,6 +110,8 @@ const GuessInput = ({
   setCounter,
   code,
   setHint,
+  winLoose,
+  setWinLoose,
 }) => {
   // console.log('first guess')
 
@@ -89,7 +120,16 @@ const GuessInput = ({
       <div>Enter guess #{counter + 1} (XXXX)</div>
       <form
         onSubmit={(e) =>
-          handleGuessSubmit(e, value, setCounter, counter, code, setHint)
+          handleGuessSubmit(
+            e,
+            value,
+            setCounter,
+            counter,
+            code,
+            setHint,
+            winLoose,
+            setWinLoose
+          )
         }
       >
         <input
@@ -104,31 +144,52 @@ const GuessInput = ({
   )
 }
 
-const handleGuessSubmit = (e, value, setCounter, counter, code, setHint) => {
+// handles submission, validations, determines win or loose
+const handleGuessSubmit = (
+  e,
+  value,
+  setCounter,
+  counter,
+  code,
+  setHint,
+  winLoose,
+  setWinLoose
+) => {
   e.preventDefault()
   console.log('code:', code)
-  // check if length is four
-  if (value.length === 4) {
-    setCounter(counter + 1)
-  }
 
   // calculate hint
   var hintCount = 0
+  if (value.length === 4) {
+    var strArr = [...code]
+    strArr.map((char, index) => {
+      console.log('char: ', char, index)
 
-  var strArr = [...code]
-  strArr.map((char, index) => {
-    console.log('char: ', char, index)
+      var vpos = value.charAt(index)
+      console.log('vpos: ', vpos)
 
-    var vpos = value.charAt(index)
-    console.log('vpos: ', vpos)
+      if (vpos === char) {
+        hintCount += 1
+      }
+    })
 
-    if (vpos === char) {
-      hintCount += 1
+    console.log('hintCount', hintCount)
+    setHint(String(hintCount))
+
+    // check if length is four
+
+    if (hintCount === 4) {
+      // u won
+      setWinLoose(true)
+      console.log('u won')
+    } else if (counter === 11) {
+      // lost
+      setWinLoose(false)
+      console.log('u lost')
+    } else {
+      setCounter(counter + 1)
     }
-  })
-
-  console.log('hintCount', hintCount)
-  setHint(String(hintCount))
+  }
 
   return console.log('submitted', value)
 }
