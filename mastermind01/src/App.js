137619b32678b8f8bc586230f740221b8d1ddbef
SwenSchaeferjohann <swen.schaeferjohann@code.berlin>
2020-06-04 12:06:45 +0200

    added proper display of guesses

--- mastermind01/src/App.js
+++ mastermind01/src/App.js
@@ -5,6 +5,7 @@ export default function App() {
 }
 
 function MasterMind() {
+  const [counter, setCounter] = useState(0)
   const [guesses, setGuesses] = useState([
     '',
     '',
@@ -28,34 +29,42 @@ function MasterMind() {
         Welcome to Mastermind! I created a random code for you. Let's get
         crackin'
       </div>
-      {guesses.map((guess, index) => (
-        <div>{guess.length === 4 && 'Guess #' + index + 1 + ': ' + guess}</div>
-      ))}
-
-      {
-        <GuessInput
-          key={0}
-          setValue={(value) => {
-            console.log('value: ', value)
-
-            let newGuess = [...guesses]
-            newGuess[0] = value
-            setGuesses(newGuess)
-          }}
-          value={guesses[0]}
-        />
-      }
+      {guesses.map((guess, index) => {
+        let number = index + 1
+        console.log(counter)
+        console.log(index)
+        return (
+          <div style={{ color: counter === index ? 'blue' : 'grey' }}>
+            {
+              // counter === index + 1 &&
+              'Guess #' + number + ': ' + guess
+            }
+          </div>
+        )
+      })}
+      <GuessInput
+        key={counter}
+        setValue={(value) => {
+          console.log('value: ', value)
+          let newGuess = [...guesses]
+          newGuess[counter] = value
+          setGuesses(newGuess)
+        }}
+        value={guesses[counter]}
+        counter={counter}
+        setCounter={setCounter}
+      />
     </Fragment>
   )
 }
 
-const GuessInput = ({ value, setValue }) => {
+const GuessInput = ({ value, setValue, counter, setCounter }) => {
   console.log('first guess')
 
   return (
     <Fragment>
-      <div>Please enter your first guess (XXXX)</div>
-      <form onSubmit={(e) => handleGuessSubmit(e, value)}>
+      <div>Enter guess #{counter + 1} (XXXX)</div>
+      <form onSubmit={(e) => handleGuessSubmit(e, value, setCounter, counter)}>
         <input
           type='text'
           onChange={(e) => setValue(e.target.value)}
@@ -68,9 +77,9 @@ const GuessInput = ({ value, setValue }) => {
   )
 }
 
-const handleGuessSubmit = (e, value) => {
+const handleGuessSubmit = (e, value, setCounter, counter) => {
   e.preventDefault()
   // check if length is four
-
+  setCounter(counter + 1)
   return console.log('submitted', value)
 }
